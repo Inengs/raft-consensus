702002d6717bbfafd939e25821bf5fccf6c7e41f
Inengiye Emmanuel <inengiyeemmanuel@gmail.com>
2026-06-02 08:03:22 +0100

    added more types

--- internal/raft/node.go
+++ internal/raft/node.go
@@ -1,6 +1,9 @@
 package raft
 
-import "sync"
+import (
+	"sync"
+	"time"
+)
 
 type NodeState int
 
@@ -28,4 +31,39 @@ type Node struct {
 	// Identity
 	id int
 	peers []string // peer HTTP addresses e.g. ["localhost:8001", "localhost:8002"]
+
+	// --- Persistent state (must survive crash) ---
+	currentTerm int
+	votedFor int // -1 means none
+	log []LogEntry
+
+	// Volatile state (all nodes)
+	commitIndex int
+	lastApplied int
+	state NodeState
+
+	// Volatile state (leader only, reset after election)
+	nextIndex []int // indexed by peer
+	matchIndex []int
+
+	// Timers
+	electionTimer *time.Timer
+	heartbeatTimer *time.Timer
+
+	// Communication
+	applyCh chan ApplyMsg // sends committed entries to the KV store
+
+	// Persistence 
+	persister Persister
+}
+
+// Apply message
+type ApplyMsg struct {
+	Command Command
+	Index int
+}
+
+type Persister struct {
+	currentTerm int
+	log []LogEntry
 }
